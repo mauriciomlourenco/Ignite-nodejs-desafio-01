5619a5908e37a3fc68f2c4242c179866066b2bb1
Mauricio Machado Lourenço <mauricio.mchd@gmail.com>
2024-06-20 22:28:40 -0300

    Solution challenge todolist API Ignite

---
+++ src/routes.js
@@ -0,0 +1,112 @@
+import { randomUUID } from 'node:crypto'
+import { buildRoutePath } from "./utils/build-route-path.js"
+import { Database } from './middlewares/database.js'
+
+const database = new Database()
+
+export const routes = [
+    {
+        method: 'GET',
+        path: buildRoutePath('/tasks'),
+        handler: (req, res) => {
+            const { search } = req.query
+
+           const tasks = database.select('tasks', search ? {
+               title: search,
+               description: search,
+           }: null)
+
+           return res.end(JSON.stringify(tasks))
+        },
+    },
+    {
+        method: 'POST',
+        path: buildRoutePath('/tasks'),
+        handler: (req, res) => {
+            const { title, description } = req.body
+
+            if(!title) {
+                return res.writeHead(400).end('Title is required')
+            }
+
+            if(!description) {
+                return res.writeHead(400).end('Description is required')
+            }
+
+            const task = {
+                id: randomUUID(),
+                title,
+                description,
+                completed_at: null,
+                created_at: new Date(),
+                updated_at: new Date(),
+            }
+
+            database.insert('tasks', task)
+
+            return res.writeHead(201).end()
+        },
+    },
+    {
+        method: 'PUT',
+        path: buildRoutePath('/tasks/:id'),
+        handler: (req, res) => {
+            const { id } = req.params
+            const { title, description } = req.body
+
+            if (!title && !description) {
+                return res.writeHead(400).end(
+                  JSON.stringify({ message: 'title or description are required' })
+                )
+              }
+
+            const [task] = database.select('tasks', { id })
+
+            if (!task) {
+                return res.writeHead(404). end()
+            }
+
+            database.update('tasks', id, {
+                title,
+                description,
+            })
+
+            return res.writeHead(204).end()
+            
+        },
+    },
+    {
+        method: 'DELETE',
+        path: buildRoutePath('/tasks/:id'),
+        handler: (req, res) => {
+           const { id } = req.params
+
+           const [task] = database.select('tasks', { id })
+
+            if (!task) {
+                return res.writeHead(404).end()
+            }
+
+           database.delete('tasks', id)
+
+           return res.writeHead(204).end()
+        },
+    },
+    {
+        method: 'PATCH',
+        path: buildRoutePath('/tasks/:id/complete'),
+        handler: (req, res) => {
+            const { id } = req.params
+
+            const [task] = database.select('tasks', { id })
+
+            if (!task) {
+                return res.writeHead(404).end()
+            }
+
+            database.complete('tasks', id)
+
+            return res.writeHead(204).end()
+        }
+    }
+]
